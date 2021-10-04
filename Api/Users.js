7e96eae62c573c3bb28ec7fc07357622d3ceaa1d
Deepak-Sharma28 <dk1999sharma1999@gmail.com>
2021-10-04 12:42:51 +0530

    added comments

--- Api/Users.js
+++ Api/Users.js
@@ -4,18 +4,28 @@ const checkOut = require('../controller/ClockOut');
 const { Tracker } = require('../controller/Tracker');
 const { check } = require('express-validator');
 
+
+//@route  Post /api/users/user/clockin
+//desc    Create a user checkin
+//access  Public
 Router
     .post('/user/clockin', [
         check('userName', "userName is required").not().isEmpty(),
         check('ClockIn', "clock in is required").not().isEmpty()
     ], ClockIn);
 
+
+//@route  PUT /api/users/user/clocklout
+//desc    Create a user checkout
+//access  Public
 Router
     .put('/user/clockout', [
         check('userName', "userName is required").not().isEmpty(),
     ], checkOut.clockOut);
 
-
+//@route  GET /api/users/user/tracker
+//desc    To tracker user's working hours
+//access  Public
 Router
     .get('/user/tracker', Tracker);
 
